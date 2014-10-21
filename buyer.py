ed4a4821bc7781b3d115556fd307064f4c3f77ab
Matt Kutz <mattkutz90@gmail.com>
2014-10-21 16:21:29 -0500

    Added buyer class and __init__.py
    added buyer.py and __init__.py

---
+++ buyer.py
@@ -0,0 +1,23 @@
+class Buyer:
+	def __init__(self)
+		self._name = None
+		self._address = None
+		self._age = None
+	
+	def name(self):
+		return self._name
+	
+	def name(self, value):
+		self._name = value
+	
+	def address(self):
+		return self._address
+		
+	def address(self, value):
+		self._address = value
+		
+	def age(self):
+		return self._age
+	
+	def age(self, value):
+		self._age = value
