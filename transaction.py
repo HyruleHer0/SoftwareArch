685c463b5b3606e088bcc6c9abc776e4bfe0237d
KnightPounds <knightpounds@gmail.com>
2014-10-28 14:06:27 -0500

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -1,1 +1,26 @@
 class Transaction:
+        def __init__(self, item):
+                self._item = item
+                self._seller = item.getseller()
+                self._highBidder = None
+                self._highBid = item.getstart()
+                
+        def getItem(self):
+                return self._item
+                
+        def setItem(self, item):
+                self._item = item
+                
+        def getSeller(self):
+                return self._seller
+                
+        def setSeller(self, seller):
+                self._seller = seller
+                
+        def submitBid(self, buyer, bid)
+                if(bid > self._highbid):
+                        self._highBidder = buyer
+                        self._highBid = bid
+                
+        def setHighBid(self, bid)
+                self._highBid = bid
