5df416967340b22b75b0ebbf6687b4acb4154163
HyruleHer0 <cok4@msstate.edu>
2014-10-28 23:51:13 -0500

    Update Item.py

--- Item.py
+++ Item.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 class Item:
     def __init__(self,name,start,desc=None):
         self._name = name
         self._start = start
         self._desc = desc
-=======
+
 from Seller import Seller
 class Item:
     def __init__(self,name,start,seller,desc=None):
@@ -12,7 +11,7 @@ class Item:
         self._start = start
         self._desc = desc
         self._seller = seller
->>>>>>> upstream/master
+
 
     def getname (self):
         return self._name
@@ -32,11 +31,9 @@ class Item:
     def desc (self, desc):
         self._desc = desc
         
-<<<<<<< HEAD
-=======
     def seller (self, seller):
         self._seller = seller
     
     def getseller (self):
         return self._seller
->>>>>>> upstream/master
+
